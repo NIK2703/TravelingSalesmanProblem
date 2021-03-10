167ba239b9fb3f2767bb74d9a0f671415fd9e296
Никита <nikita.ogurnoy@mail.ru>
2021-03-10 20:38:32 +0600

    корректная работа программы на тестовом примере

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,12 +7,84 @@
 #include <string>
 using namespace std;
 
+int transformIndex(int source_index, vector<int> index_list);
+
 template<class Type>
 void print_matrix(Type** matrix, int size_x, int size_y) {
 	for (int i = 0; i < size_x; i++) {
 		for (int j = 0; j < size_y; j++) {
 			cout << matrix[i][j];
 			if (j != size_y - 1) {
+				cout << "\t\t";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+template<class Type> 
+Type** clone_matrix(Type** matrix, int size_x, int size_y) {
+	Type** new_matrix = new Type*[size_x];
+	for (int i = 0; i < size_x; i++) {
+		new_matrix[i] = new Type[size_y];
+		for (int j = 0; j < size_y; j++) {
+			new_matrix[i][j] = matrix[i][j];
+		}
+	}
+	return new_matrix;
+}
+
+template <class Type>
+struct numbered_matrix {
+	Type** matrix;
+	vector<int> out_indexes;
+	vector<int> in_indexes;
+};
+
+template <class Type>
+numbered_matrix<Type> remove_edge(numbered_matrix<Type> n_matrix, int* edge) {
+	vector<int> new_out_indexes(n_matrix.out_indexes);
+	vector<int> new_in_indexes(n_matrix.in_indexes);
+	new_out_indexes.erase(new_out_indexes.begin() + transformIndex(edge[0], new_out_indexes));
+	new_in_indexes.erase(new_in_indexes.begin() + transformIndex(edge[1], new_in_indexes));
+	Type** new_matrix = new Type * [n_matrix.out_indexes.size()];
+	for (int i = 0; i < new_out_indexes.size(); i++) {
+		new_matrix[i] = new Type[n_matrix.in_indexes.size()];
+		for (int j = 0; j < new_in_indexes.size(); j++) {
+			new_matrix[i][j] = n_matrix.matrix[transformIndex(new_out_indexes[i], n_matrix.out_indexes)][transformIndex(new_in_indexes[j], n_matrix.in_indexes)];
+		}
+	}
+	n_matrix.out_indexes = new_out_indexes;
+	n_matrix.in_indexes = new_in_indexes;
+	//printf("%i %i", transformIndex(edge[1], n_matrix.out_indexes), transformIndex(edge[1], n_matrix.in_indexes));
+	new_matrix[transformIndex(edge[1], n_matrix.out_indexes)][transformIndex(edge[0], n_matrix.in_indexes)] = numeric_limits<Type>::max();
+	n_matrix.matrix = new_matrix;
+	return n_matrix;
+}
+
+template <class Type>
+numbered_matrix<Type> clone_numbered_matrix(numbered_matrix<Type> n_matrix) {
+	numbered_matrix<Type> new_n_matrix;
+	new_n_matrix.matrix = clone_matrix(n_matrix.matrix, n_matrix.out_indexes.size(), n_matrix.in_indexes.size());
+	new_n_matrix.out_indexes = vector<Type>(n_matrix.out_indexes);
+	new_n_matrix.in_indexes = vector<Type>(n_matrix.in_indexes);
+	return new_n_matrix;
+}
+
+template<class Type>
+void print_numbered_matrix(numbered_matrix<Type> n_matrix) {
+	cout << "\t\t";
+	for (int i = 0; i < n_matrix.in_indexes.size(); i++) {
+		cout << n_matrix.in_indexes.at(i);
+		cout << "\t\t";
+	}
+	cout << "\n";
+	for (int i = 0; i < n_matrix.out_indexes.size(); i++) {
+		cout << n_matrix.out_indexes.at(i);
+		cout << "\t\t";
+		for (int j = 0; j < n_matrix.in_indexes.size(); j++) {
+			cout << n_matrix.matrix[i][j];
+			if (j != n_matrix.in_indexes.size() - 1) {
 				cout << "\t\t";
 			}
 		}
@@ -28,10 +100,10 @@ float distance(float* p1, float* p2) {
 float** generateFlatGraph(int n) {
 	//параметры генератора случайных чисел
 	const float accuracy = 0.01;
-	const int divider = (int) 1 / accuracy;
+	const int divider = (int)1 / accuracy;
 
 	//генерация координат точек
-	float** point_coord = new float*[n];
+	float** point_coord = new float* [n];
 	for (int i = 0; i < n; i++) {
 		point_coord[i] = new float[2];
 		point_coord[i][0] = (float)(rand() % (divider + 1)) / divider;
@@ -39,7 +111,7 @@ float** generateFlatGraph(int n) {
 	}
 
 	//вычисление матрицы смежности графа на основе расстояний между точками
-	float** adj_matrix = new float*[n];
+	float** adj_matrix = new float* [n];
 	for (int i = 0; i < n; i++) {
 		adj_matrix[i] = new float[n];
 		for (int j = 0; j < n; j++) {
@@ -97,7 +169,7 @@ Type reduce_column(Type** matrix, int column, int size, Type val) {
 template <class Type>
 Type reduce_matrix(Type** matrix, int size_x, int size_y) {
 	Type val = 0;
-	for (int i = 0; i < size_x; i++) { 
+	for (int i = 0; i < size_x; i++) {
 		val += reduce(matrix[i], size_x, min(matrix[i], size_x));
 	}
 	for (int i = 0; i < size_y; i++) {
@@ -112,7 +184,7 @@ vector<int*> matrix_null_element_positions(Type** matrix, int size) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			if (matrix[i][j] == 0) {
-				int* pos = new int[2]{i,j};
+				int* pos = new int[2]{ i,j };
 				null_element_positions.push_back(pos);
 			}
 		}
@@ -130,15 +202,15 @@ Type zero_degree(Type** matrix, int* pos, int n) {
 }
 
 template <class Type>
-Type max_zero_degree_null(Type** matrix, int* pos, int n) {
+Type max_zero_degree_null(numbered_matrix<Type> n_matrix, int* pos) {
 	Type max = numeric_limits<Type>::min();
-	vector<int*> null_positions = matrix_null_element_positions(matrix, n);
+	vector<int*> null_positions = matrix_null_element_positions(n_matrix.matrix, n_matrix.out_indexes.size());
 	while (!null_positions.empty()) {
-		Type val = zero_degree(matrix, null_positions.back(), n);
+		Type val = zero_degree(n_matrix.matrix, null_positions.back(), n_matrix.out_indexes.size());
 		if (val > max) {
 			max = val;
-			pos[0] = null_positions.back()[0];
-			pos[1] = null_positions.back()[1];
+			pos[0] = n_matrix.out_indexes[null_positions.back()[0]];
+			pos[1] = n_matrix.in_indexes[null_positions.back()[1]];
 			null_positions.back();
 		}
 		null_positions.pop_back();
@@ -173,7 +245,7 @@ Type** matrix_from_file(string path) {
 			matrix_lines.push_back(matrix_line);
 		}
 		in.close();
-		Type** matrix = new Type*[matrix_lines.size()];
+		Type** matrix = new Type * [matrix_lines.size()];
 		for (int i = 0; i < matrix_lines.size(); i++) {
 			matrix[i] = new Type[matrix_lines.at(i).size()];
 			for (int j = 0; j < matrix_lines.at(i).size(); j++) {
@@ -185,17 +257,171 @@ Type** matrix_from_file(string path) {
 	return NULL;
 }
 
+vector<int> generateIndexList(int n) {
+	vector<int> indexes;
+	for (int i = 0; i < n; i++) {
+		indexes.push_back(i);
+	}
+	return indexes;
+}
+
 template <class Type>
-struct tree_node {
+void print_vector(vector<Type> vector) {
+	for (int i = 0; i < vector.size(); i++) {
+		printf("%i ",vector.at(i));
+	}
+	printf("\n");
+}
+
+int transformIndex(int source_index, vector<int> index_list) { //приведение индексов после удаления рёбер из матрицы смежности
+	int new_index;
+	new_index = -1;
+	for (int i = 0; i < index_list.size(); i++) {
+		if (source_index == index_list.at(i)) {
+			new_index = i;
+		}
+	}
+	return new_index;
+}
+
+template <class T>
+struct node_alg_little {
+	numbered_matrix<T> adj_matrix;
 	int* edge;
-	Type val;
+	T val;
 };
+
+template <class Type>
+struct binary_tree_node {
+	Type content;
+	binary_tree_node* left;
+	binary_tree_node* right;
+	binary_tree_node* parent;
+};
+
+template <class Type>
+binary_tree_node<node_alg_little<Type>>* alg_little_step(binary_tree_node<node_alg_little<Type>>* node) {
+	int* edge = new int[2];
+	max_zero_degree_null(node->content.adj_matrix, edge);
+
+	binary_tree_node<node_alg_little<int>> left_node;
+	left_node.content.adj_matrix = remove_edge(clone_numbered_matrix(node->content.adj_matrix), edge);
+	left_node.content.edge = edge;
+	print_numbered_matrix(left_node.content.adj_matrix);
+	cout << endl << endl;
+	left_node.content.val = node->content.val + reduce_matrix(left_node.content.adj_matrix.matrix,
+		left_node.content.adj_matrix.out_indexes.size(), left_node.content.adj_matrix.in_indexes.size());
+	left_node.parent = node;
+	node->left = &left_node;
+
+	cout << left_node.content.edge[0] << ' ' << left_node.content.edge[1] << endl;
+	cout << left_node.content.val << endl;
+	print_numbered_matrix(left_node.content.adj_matrix);
+	cout << endl << endl;
+	
+	binary_tree_node<node_alg_little<int>> right_node;
+	right_node.content.adj_matrix = clone_numbered_matrix(node->content.adj_matrix);
+	right_node.content.adj_matrix.matrix[transformIndex(edge[0], right_node.content.adj_matrix.out_indexes)]
+		[transformIndex(edge[1], right_node.content.adj_matrix.in_indexes)] = numeric_limits<Type>::max();
+	right_node.content.edge = edge;
+	print_numbered_matrix(left_node.content.adj_matrix);
+	cout << endl << endl;
+	right_node.content.val = node->content.val + reduce_matrix(right_node.content.adj_matrix.matrix,
+		right_node.content.adj_matrix.out_indexes.size(), right_node.content.adj_matrix.in_indexes.size());
+	right_node.parent = node;
+	node->right = &right_node;
+
+	cout << right_node.content.edge[0] << ' ' << right_node.content.edge[1] << endl;
+	cout << right_node.content.val << endl;
+	print_numbered_matrix(right_node.content.adj_matrix);
+	cout << endl << endl;
+
+	binary_tree_node<node_alg_little<Type>>* nodes = new binary_tree_node<node_alg_little<Type>>[2];
+	nodes[0] = left_node;
+	nodes[1] = right_node;
+	return nodes;
+}
+
+template <class Type>
+vector<int> traveling_saleman_problem_solution_alg_little(numbered_matrix<Type> adj_matrix) {
+	vector<binary_tree_node<node_alg_little<Type>>*> leaf_list;
+	binary_tree_node<node_alg_little<Type>> root;
+	root.content.adj_matrix = clone_numbered_matrix(adj_matrix);
+	root.content.val = reduce_matrix(root.content.adj_matrix.matrix, root.content.adj_matrix.out_indexes.size(),
+		root.content.adj_matrix.in_indexes.size());
+	root.parent = NULL;
+
+	Type min_val = numeric_limits<Type>::max();
+	binary_tree_node<node_alg_little<Type>>* next_node = &root;
+	while (next_node->content.adj_matrix.out_indexes.size() + next_node->content.adj_matrix.in_indexes.size() > 2) {
+		binary_tree_node<node_alg_little<Type>>* nodes = alg_little_step(next_node);
+		next_node->left = &nodes[0];
+		next_node->right = &nodes[1];
+		leaf_list.push_back(&nodes[0]);
+		leaf_list.push_back(&nodes[1]);
+		int min_i = 0;
+		for (int i = 0; i < leaf_list.size(); i++) {
+			if (leaf_list[i]->content.val < min_val) {
+				min_val = leaf_list[i]->content.val;
+				next_node = leaf_list[i];
+				min_i = i;
+			}
+		}
+		leaf_list.erase(leaf_list.begin() + min_i);
+		min_val = numeric_limits<Type>::max();
+	}
+	vector<int*> edges;
+	int* edge = new int[2];
+	edge[0] = next_node->content.adj_matrix.out_indexes[0];
+	edge[1] = next_node->content.adj_matrix.in_indexes[0];
+	edges.push_back(edge);
+	while (next_node->parent != NULL) {
+		if (next_node->parent->right != next_node) {
+			edges.push_back(next_node->content.edge);
+		}
+		next_node = next_node->parent;
+	}
+	for (int i = 0; i < edges.size(); i++) {
+		cout << edges[i][0] << edges[i][1] << endl;
+	}
+	vector<int> path;
+	int next_vertex = 0;
+	path.push_back(next_vertex);
+	while (edges.empty() != true) {
+		for (int i = 0; i < edges.size(); i++) {
+			if (edges[i][0] == next_vertex) {
+				next_vertex = edges[i][1];
+				edges.erase(edges.begin() + i);
+				break;
+			};
+		}
+		path.push_back(next_vertex);
+	}
+	
+	return path;
+}
 
 int main()
 {
 	const int n = 5;
 	//float** matrix = generateFlatGraph(n);
 
-	int** matrix = matrix_from_file<int>("input.txt");
-	
+	//int** matrix = matrix_from_file<int>("input.txt");
+	numbered_matrix<int> n_matrix;
+	n_matrix.matrix = matrix_from_file<int>("input.txt");
+	n_matrix.out_indexes = generateIndexList(n);
+	n_matrix.in_indexes = generateIndexList(n);
+
+	vector<int> path = traveling_saleman_problem_solution_alg_little(n_matrix);
+	for (int i = 0; i < path.size(); i++) {
+		cout << path[i] << ' ';
+	}
+
+	/*binary_tree_node<int> root;
+	root.val = h;
+
+	n_matrix = remove_edge<int>(n_matrix, edge);
+	int h2 = reduce_matrix(n_matrix.matrix, n_matrix.out_indexes.size(), n_matrix.in_indexes.size());
+	cout << h2;*/
 }
+
